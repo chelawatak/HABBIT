5d3f98cfa05b99dfaefb2ee4a8d25693b0ce4d79
Animesh Jain <ajanimesh20026@gmail.com>
2023-10-31 00:46:54 +0530

    adjusted the width

--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -46,7 +46,7 @@ const RowContainer = ({ flag, data, scrollValue }) => {
           <div
             key={item?.id}
             className="w-275 h-[175px] min-w-[275px] md:w-300 md:min-w-[300px]  bg-cardOverlay rounded-lg py-2 px-4  my-12 backdrop-blur-lg hover:drop-shadow-lg flex flex-col items-center justify-evenly relative"
-            style={{minWidth: "400px", height: "200px"}}
+            style={{minWidth: "380px", height: "200px"}}
           >
             <div className="w-full flex items-center justify-between">
               <motion.div
