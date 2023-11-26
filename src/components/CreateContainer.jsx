768fccef1958c0bc685bb0f4324440c69f20eaea
chelawatak <chelawatak@gmail.com>
2023-11-26 20:12:09 +0530

    given access of adding items to shopkeepers

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -148,7 +148,7 @@ const CreateContainer = () => {
 
   return (
     <div className="w-full min-h-screen flex items-center justify-center">
-      {user && user.email === "habbit.lnmiit@gmail.com" && <div className="w-[90%] md:w-[50%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center gap-4">
+      {user && (user.email === "habbit.lnmiit@gmail.com" || user.email === "amulparlour.lnmiit@gmail.com" || user.email === "babajuice.lnmiit@gmail.com"  || user.email === "vinayakfoods.lnmiit@gmail.com") && <div className="w-[90%] md:w-[50%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center gap-4">
         {fields && (
           <motion.p
             initial={{ opacity: 0 }}
